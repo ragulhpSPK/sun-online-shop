6669e361fe93bf05bd1108cdf220debc349fe395
ragul0427 <ragulhp2704@gmail.com>
2023-04-06 23:14:31 +0530

    top

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Swiper from "../components/swiper";
 import Delivery from "@/components/delivery";
 import Categories from "@/components/categories";
 import Bestdeals from "@/components/bestdeals";
+import Topproducts from "@/components/topproducts";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
         <Delivery />
         <Categories />
         <Bestdeals/>
+        <Topproducts/>
       </main>
     </>
   );
